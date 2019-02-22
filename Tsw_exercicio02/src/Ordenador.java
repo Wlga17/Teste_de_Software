f8d5119e746c884a17c767082436f46741cb83b5
William Luiz <williamlga@gmail.com>
2019-02-22 15:27:45 -0300

    Implementando a classe Ordenador.

---
+++ Tsw_exercicio02/src/Ordenador.java
@@ -0,0 +1,38 @@
+import java.util.Collection;
+
+public class Ordenador {
+	
+	private Double maiorValor;
+	private Double menorValor;
+	private Double valorMedio;
+	private Double valorMediana;
+	
+	public Ordenador() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void inserirColecao(Collection<String> pColecao) {
+		
+	}
+	
+	public void informarValor(String pValor) {
+		
+	}
+	
+	public Double getMaiorValor() {
+		return maiorValor;
+	}
+
+	public Double getMenorValor() {
+		return menorValor;
+	}
+
+	public Double getValorMedio() {
+		return valorMedio;
+	}
+
+	public Double getValorMediana() {
+		return valorMediana;
+	}
+	
+}
