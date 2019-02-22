04339c87bb1cbacfc343f6a8ebab1f96b1fe49db
William Luiz <williamlga@gmail.com>
2019-02-22 02:34:12 -0300

    Atualização do Exercício 01.

--- Tsw_exercicio01/src/Calculadora.java
+++ Tsw_exercicio01/src/Calculadora.java
@@ -4,20 +4,33 @@ public class Calculadora implements ICalculadora{
 	@Override
 	public int somar(String v1, String v2) {
 		int result = 0;
-		result = Integer.parseInt(v1) + Integer.parseInt(v2);
+		int valor1 = Integer.parseInt(v1);
+		int valor2 = Integer.parseInt(v2); 
+		if(v1 == "" || v2 == "") {
+			throw new RuntimeException("Não é possivel somar sem dois valores");
+		}
+		result = valor1 + valor2;
 		return result;
 	}
 
 	@Override
 	public double dividir(String v1, String v2) {
 		double result = 0;
-		result = Double.parseDouble(v1) / Double.parseDouble(v2);
+		double valor1 = Double.parseDouble(v1);
+		double valor2 = Double.parseDouble(v2);
+		if(v1 == "" || v2 == "") {
+			throw new RuntimeException("Não é possivel dividir sem dois valores");
+		}
+		result = valor1 / valor2;
 		return result;
 	}
 
 	@Override
 	public double raizQuadrada(String v) {
 		double valor = Double.parseDouble(v);
+		if(v == "") {
+			throw new RuntimeException("Não é possivel fazer a raiz quadrada sem valor");
+		}
 		double result = Math.pow(valor, 0.5);
 		return result;
 	}
--- Tsw_exercicio01/teste/Teste.java
+++ Tsw_exercicio01/teste/Teste.java
@@ -1,28 +1,88 @@
 import org.junit.Test;
 
-import org.junit.After;
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Ignore;
 
 public class Teste {
 
-	Calculadora c = new Calculadora();
+	private Calculadora c;
+	
+	@Before
+	public void criaCalculadora() {
+		this.c = new Calculadora();
+	}
 	
 	@Test
-	public void testeSomar() {
-		int resultado = c.somar("1", "3");
-		Assert.assertEquals(4, resultado);
+	public void testeSomarComNumerosPositivos() {
+		String valor1 = "1";
+		String valor2 = "2";
+		int resultado = c.somar(valor1,valor2);
+		int valorEsperado = 3;
+		assertEquals(valorEsperado, resultado);
 	}
 	
-	@Ignore
-	public void testeDividir() {
-		double resultado = c.dividir("4", "2");
+	@Test
+	public void testeSomarComNumerosNegativos() {
+		String valor1 = "-3";
+		String valor2 = "-4";
+		int resultado = c.somar(valor1,valor2);
+		int valorEsperado = -7;
+		assertEquals(valorEsperado, resultado);
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void testeSomarSemValor() {
+		String valor1 = "2";
+		String valor2 = "";
+		int resultado = c.somar(valor1,valor2);
+	}
+	
+	@Test
+	public void testeDividir1Por10() {
+		String valor1 = "1";
+		String valor2 = "10";
+		double valorEsperado = 0.1;
+		double resultado = c.dividir(valor1,valor2);
+		assertEquals(valorEsperado, resultado, 0);
+	}
+
+	@Test
+	public void testeDividir10Por1() {
+		String valor1 = "10";
+		String valor2 = "1";
+		double valorEsperado = 10;
+		double resultado = c.dividir(valor1,valor2);
+		assertEquals(valorEsperado, resultado, 0);
+	}
+
+	@Test
+	public void testeDividirValoresNaoInteiro() {
+		String valor1 = "2.57";
+		String valor2 = "5.29";
+		double valorEsperado = 0.48582230623818523;
+		double resultado = c.dividir(valor1,valor2);
+		assertEquals(valorEsperado, resultado, 0);
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void testeDividirSemValor() {
+		String valor1 = "";
+		String valor2 = "10";
+		double resultado = c.dividir(valor1,valor2);
+	}
 		
+	@Test(expected=RuntimeException.class)
+	public void testeRaizQuadradaSemValor() {
+		String valor1 = "";
+		double resultado = c.raizQuadrada(valor1);
 	}
 	
-	@Ignore
-	public void testeRaizQuadrada() {
-		double resultado = c.raizQuadrada("9");
+	@Test
+	public void testeRaizQuadradaComValor() {
+		String valor1 = "9";
+		double valorEsperado = 3;
+		double resultado = c.raizQuadrada(valor1);
+		assertEquals(valorEsperado, resultado, 0);
 	}
 }
